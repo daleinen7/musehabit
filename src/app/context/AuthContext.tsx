a15d9b690cbf45411d3c0b4d30504985b0edad31
Doug Leinen <doug@geletkaplus.com>
2024-03-03 00:21:30 -0600

    type user

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -61,7 +61,7 @@ export const AuthContextProvider = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<UserType | null>(null);
   const [loading, setLoading] = useState(true);
 
   const router = useRouter();
